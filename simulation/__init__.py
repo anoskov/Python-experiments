94b48dabf718812444dfd429eab647c06b2f2d3a
anoskov <flashbulb54@gmail.com>
2014-10-27 16:02:09 +0300

    Drunken walks

---
+++ simulation/__init__.py
@@ -0,0 +1,32 @@
+from Drunk import UsualDrunk
+from Location import Location
+from Field import Field
+
+def walk(f, d, numSteps):
+    start = f.getLoc(d)
+    for s in range(numSteps):
+        f.moveDrunk(d)
+    return(start.distFrom(f.getLoc(d)))
+
+
+
+def simWalks(numSteps, numTrials):
+    homer = UsualDrunk('Homer')
+    origin = Location(0, 0)
+    distances = []
+    for t in range(numTrials):
+        f = Field()
+        f.addDrunk(homer, origin)
+        distances.append(walk(f, homer, numSteps))
+    return distances
+
+
+
+def drunkTest(numTrials = 20):
+    for numSteps in [10, 100, 1000, 10000]:
+        distances = simWalks(numSteps, numTrials)
+        print 'Random walk of ' + str(numSteps) + ' steps'
+        print ' Mean =', sum(distances)/len(distances)
+        print ' Max =', max(distances), 'Min =', min(distances)
+
+drunkTest()
