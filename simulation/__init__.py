a74aa1e2ced7f7047124f2d12f7165ea7f511511
anoskov <flashbulb54@gmail.com>
2014-10-29 00:17:35 +0300

    Added several drunk types and visualisation

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -1,7 +1,7 @@
-from Drunk import UsualDrunk
+from Drunk import *
 from Location import Location
 from Field import Field
-
+import pylab
 def walk(f, d, numSteps):
     start = f.getLoc(d)
     for s in range(numSteps):
@@ -10,8 +10,8 @@ def walk(f, d, numSteps):
 
 
 
-def simWalks(numSteps, numTrials):
-    homer = UsualDrunk('Homer')
+def simWalks(numSteps, numTrials, dClass):
+    homer = dClass('Homer')
     origin = Location(0, 0)
     distances = []
     for t in range(numTrials):
@@ -21,7 +21,6 @@ def simWalks(numSteps, numTrials):
     return distances
 
 
-
 def drunkTest(numTrials = 20):
     for numSteps in [10, 100, 1000, 10000]:
         distances = simWalks(numSteps, numTrials)
@@ -29,4 +28,20 @@ def drunkTest(numTrials = 20):
         print ' Mean =', sum(distances)/len(distances)
         print ' Max =', max(distances), 'Min =', min(distances)
 
-drunkTest()
+def drunkTestP(numTrials = 50):
+    stepsTaken = [10, 100, 1000, 10000]
+    for dClass in (UsualDrunk, ColdDrunk, EDrunk):
+        meanDistances = []
+        for numSteps in stepsTaken:
+            distances = simWalks(numSteps, numTrials, dClass)
+            meanDistances.append(sum(distances)/len(distances))
+        pylab.plot(stepsTaken, meanDistances,
+                   label = dClass.__name__)
+        pylab.title('Mean Distance from Origin')
+        pylab.xlabel('Steps Taken')
+        pylab.ylabel('Steps from Origin')
+        pylab.legend(loc = 'upper left')
+    pylab.show()
+
+if __name__ == "__main__":
+    drunkTestP()
