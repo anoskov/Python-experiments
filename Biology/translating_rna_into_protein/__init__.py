1d2a3367011a717f05fdf5b16d46d1a87ef254ce
anoskov <flashbulb54@gmail.com>
2014-11-04 20:14:12 +0300

    translating_rna_into_protein

---
+++ Biology/translating_rna_into_protein/__init__.py
@@ -0,0 +1,30 @@
+def group_data(text, n):
+    return [text[i:i+n] for i in range(0, len(text), n)]
+
+
+def translate_rna_to_protein(data):
+    return ''.join([rna_codon_table[seg] for seg in data if rna_codon_table[seg] != 'Stop'])
+
+
+if __name__ == "__main__":
+    rna_codon_table = {
+        'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+        'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+        'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+        'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+        'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+        'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+        'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+        'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+        'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+        'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+        'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+        'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+        'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+        'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+        'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+        'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
+    }
+    data_set = open('rna.txt')
+    data_text = data_set.read()
+    print translate_rna_to_protein(group_data(data_text, 3))
